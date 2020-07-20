c0bd6b78a8be239239f45e7f0c7b85ca1ccc9b3e
ahwingo <ahwingo@bitbucket.org>
2020-07-20 18:59:00 -0500

    Updated displays and added images.

--- canvas_files/draw_board.js
+++ canvas_files/draw_board.js
@@ -8,7 +8,60 @@
 var	canvas = document.querySelector("canvas");
 var context = canvas.getContext("2d");
 
-var drawing_started = false;  // Use this to prevent certain things from being drawn too early.
+//----------------------------------------------------------------------------------------------------------------------
+// Load all of the card images into memory.
+//----------------------------------------------------------------------------------------------------------------------
+
+var brick_card_image = new Image();
+brick_card_image.src = "./images/cards/brick.png";
+
+var ore_card_image = new Image();
+ore_card_image.src = "./images/cards/ore.png";
+
+var sheep_card_image = new Image();
+sheep_card_image.src = "./images/cards/sheep.png";
+
+var wheat_card_image = new Image();
+wheat_card_image.src = "./images/cards/wheat.png";
+
+var wood_card_image = new Image();
+wood_card_image.src = "./images/cards/wood.png";
+
+var knight_card_image = new Image();
+knight_card_image.src = "./images/cards/knight.png";
+
+var monopoly_card_image = new Image();
+monopoly_card_image.src = "./images/cards/monopoly.png";
+
+var year_of_plenty_card_image = new Image();
+year_of_plenty_card_image.src = "./images/cards/year_of_plenty.png";
+
+var road_building_card_image = new Image();
+road_building_card_image.src = "./images/cards/road_building.png";
+
+var victory_point_card_image = new Image();
+victory_point_card_image.src = "./images/cards/victory_point.png";
+
+// Determine the resource layout.
+var random_cards = ["ore", "ore", "ore",
+                           "brick", "brick", "brick",
+                           "wood", "wood", "wood", "wood",
+                           "sheep", "sheep", "sheep", "sheep",
+                           "wheat", "wheat", "wheat", "wheat",
+			   "knight", "knight", "knight",
+			   "victory point", "victory point", "victory point", 
+			   "year of plenty","year of plenty","year of plenty",
+			   "monopoly", "monopoly", "monopoly", "monopoly", 
+			   "road building", "road building", "road building", 
+			];
+                           
+
+for (let i = random_cards.length - 1; i > 0; i--) {
+const j = Math.floor(Math.random() * i);
+const temp = random_cards[i];
+random_cards[i] = random_cards[j];
+random_cards[j] = temp;
+}
 
 //----------------------------------------------------------------------------------------------------------------------
 // Load all of the resource type gifs into memory. Store the in arrays for use later.
@@ -130,6 +183,11 @@ chalk_image.onload = function() {
     chalk_pattern = context.createPattern(chalk_image, "repeat");
 }
 
+
+var options_image = new Image();
+options_image.src = "./images/options.png"
+
+
 //----------------------------------------------------------------------------------------------------------------------
 // This function will load the game state from the game state file on the server.
 //----------------------------------------------------------------------------------------------------------------------
@@ -145,26 +203,159 @@ function load_game_state()
 // These functions will display cards for the corresponding players, based on the game state.
 //----------------------------------------------------------------------------------------------------------------------
 
-function draw_left_side_cards(canvas, context, num_regular, num_development)
+function draw_top_side_cards(num_regular, num_development)
     {
-    var card_width = canvas.width * 23 / 490;
-    var card_length = canvas.height * 15 / 275;
-
+    context.fillStyle = nametag_pattern;
+    context.strokeStyle = nametag_border_pattern;
+    context.lineWidth = 2;
+    var card_width = canvas.width * 50 / 1920;
+    var card_height = canvas.height * 75 / 1080;
+    var card_spacing = canvas.width * 16 / 1920;
+    var first_row_y_offset = canvas.height * 16 / 1080;
+    var second_row_y_offset = first_row_y_offset + 0.5*card_height;
+    var total_cards = num_regular + num_development;
+    var num_first_row = Math.ceil(total_cards / 2.0);
+    var num_second_row = Math.floor(total_cards / 2.0);
+    var first_row_x_offset = (canvas.width - num_first_row*card_width - (num_first_row - 1)*card_spacing) / 2;
+    var second_row_x_offset = 0.5 * card_width + first_row_x_offset;
+    for (var i = 0; i < num_first_row; i++)
+        {
+        var card_type = random_cards[i % random_cards.length];
+        var image_type;
+        if (card_type === "brick") { image_type = brick_card_image; }
+        else if (card_type === "wheat") { image_type = wheat_card_image; }
+        else if (card_type === "ore") { image_type = ore_card_image; }
+        else if (card_type === "sheep") { image_type = sheep_card_image; }
+        else if (card_type === "wood") { image_type = wood_card_image; }
+        else if (card_type === "knight") { image_type = knight_card_image; }
+        else if (card_type === "monopoly") { image_type = monopoly_card_image; }
+        else if (card_type === "victory point") { image_type = victory_point_card_image; }
+        else if (card_type === "road building") { image_type = road_building_card_image; }
+        else if (card_type === "year of plenty") { image_type = year_of_plenty_card_image; }
+        var x_value = first_row_x_offset + i * (card_width + card_spacing);
+    	context.drawImage(image_type,
+                      dx=x_value, dy=first_row_y_offset,
+                      dWidth=card_width, dHeight=card_height);
+        context.strokeRect(x_value, first_row_y_offset, card_width, card_height);
+        }
+    for (var i = 0; i < num_second_row; i++)
+        {
+        var card_type = random_cards[i % random_cards.length];
+        var image_type;
+        if (card_type === "brick") { image_type = brick_card_image; }
+        else if (card_type === "wheat") { image_type = wheat_card_image; }
+        else if (card_type === "ore") { image_type = ore_card_image; }
+        else if (card_type === "sheep") { image_type = sheep_card_image; }
+        else if (card_type === "wood") { image_type = wood_card_image; }
+        else if (card_type === "knight") { image_type = knight_card_image; }
+        else if (card_type === "monopoly") { image_type = monopoly_card_image; }
+        else if (card_type === "victory point") { image_type = victory_point_card_image; }
+        else if (card_type === "road building") { image_type = road_building_card_image; }
+        else if (card_type === "year of plenty") { image_type = year_of_plenty_card_image; }
+        var x_value = second_row_x_offset + i * (card_width + card_spacing);
+    	context.drawImage(image_type,
+                      dx=x_value, dy=second_row_y_offset,
+                      dWidth=card_width, dHeight=card_height);
+        context.strokeRect(x_value, second_row_y_offset, card_width, card_height);
+        }
     }
+
+function draw_bottom_side_cards(num_regular, num_development)
+{
+    context.fillStyle = nametag_pattern;
+    context.strokeStyle = nametag_border_pattern;
+    context.lineWidth = 2;
+    var card_width = canvas.width * 50 / 1920;
+    var card_height = canvas.height * 75 / 1080;
+    var card_spacing = canvas.width * 16 / 1920;
+    var first_row_y_offset = canvas.height - canvas.height * 16 / 1080 - card_height;
+    var second_row_y_offset = first_row_y_offset - 0.5*card_height;
+    var total_cards = num_regular + num_development;
+    var num_first_row = Math.ceil(total_cards / 2.0);
+    var num_second_row = Math.floor(total_cards / 2.0);
+    var first_row_x_offset = (canvas.width - num_first_row*card_width - (num_first_row - 1)*card_spacing) / 2;
+    var second_row_x_offset = 0.5 * card_width + first_row_x_offset;
+    for (var i = 0; i < num_first_row; i++)
+    {
+        var x_value = first_row_x_offset + i * (card_width + card_spacing);
+        context.fillRect(x_value, first_row_y_offset, card_width, card_height);
+        context.strokeRect(x_value, first_row_y_offset, card_width, card_height);
+    }
+    for (var i = 0; i < num_second_row; i++)
+    {
+        var x_value = second_row_x_offset + i * (card_width + card_spacing);
+        context.fillRect(x_value, second_row_y_offset, card_width, card_height);
+        context.strokeRect(x_value, second_row_y_offset, card_width, card_height);
+    }
+}
+
+function draw_left_side_cards(num_regular, num_development) {
+    context.fillStyle = nametag_pattern;
+    context.strokeStyle = nametag_border_pattern;
+    context.lineWidth = 2;
+    var card_width = canvas.width * 50 / 1920;
+    var card_height = canvas.height * 75 / 1080;
+    var card_spacing = canvas.width * 16 / 1920;
+    var first_row_y_offset = (canvas.height / 2) - 2.5 * card_height - 2 * card_spacing;
+    var row_offsets = [];
+    for (var i = 0; i < 5; i++)
+        {
+        row_offsets.push(first_row_y_offset + i*(card_spacing + card_height));
+        }
+    var column_offsets = [];
+    for (var i = 0; i < 5; i++)
+        {
+        column_offsets.push(4*card_spacing + i*(card_spacing + card_width));
+        }
+    for (var i = 0; i < num_regular + num_development; i++)
+        {
+        var row_idx = Math.floor(i / 5);
+        var col_idx = i % 5;
+        context.fillRect(column_offsets[col_idx], row_offsets[row_idx], card_width, card_height);
+        context.strokeRect(column_offsets[col_idx], row_offsets[row_idx], card_width, card_height);
+        }
+}
+
+function draw_right_side_cards(num_regular, num_development) {
+    context.fillStyle = nametag_pattern;
+    context.strokeStyle = nametag_border_pattern;
+    context.lineWidth = 2;
+    var card_width = canvas.width * 50 / 1920;
+    var card_height = canvas.height * 75 / 1080;
+    var card_spacing = canvas.width * 16 / 1920;
+    var first_row_y_offset = (canvas.height / 2) - 2.5 * card_height - 2 * card_spacing;
+    var row_offsets = [];
+    for (var i = 0; i < 5; i++)
+    {
+        row_offsets.push(first_row_y_offset + i*(card_spacing + card_height));
+    }
+    var column_offsets = [];
+    for (var i = 0; i < 5; i++)
+    {
+        column_offsets.push(canvas.width - (4*card_spacing + i*(card_spacing + card_width) + card_width));
+    }
+    for (var i = 0; i < num_regular + num_development; i++)
+    {
+        var row_idx = Math.floor(i / 5);
+        var col_idx = i % 5;
+        context.fillRect(column_offsets[col_idx], row_offsets[row_idx], card_width, card_height);
+        context.strokeRect(column_offsets[col_idx], row_offsets[row_idx], card_width, card_height);
+    }
+}
 
 //----------------------------------------------------------------------------------------------------------------------
 // This block will load the cup and plate icons for each player.
 //----------------------------------------------------------------------------------------------------------------------
 
 // For the left and right player, we need their names to be vertical.
-function write_vertical_text(player_id, center_x, center_y)
+function write_vertical_text(player_id, x_offset, y_offset)
     {
     context.save();
-    context.font = "30pt arial"
+    context.rotate(Math.PI / 2.0);
+    console.log("canvas width: ", canvas.width);
     var new_name = player_id[0] + ". " + player_id.slice(1);
-    context.translate(canvas.height, canvas.width);
-    context.rotate(-0.5*Math.PI);
-    context.fillText(new_name, center_x, center_y);
+    context.fillText(new_name, x_offset, -y_offset);
+    context.fillRect(0, 0, canvas.width, canvas.height);
     context.restore();
     }
 
@@ -260,7 +451,6 @@ window.onload = function(){
 	var counter = 0;  // Use this counter to access the relevant frame number for the hex patterns.
 	setInterval( function() {
 
-	    drawing_started = true;
 
         context.fillStyle = table_top_pattern;
         context.fillRect(0, 0, canvas.width, canvas.height);
@@ -442,14 +632,10 @@ window.onload = function(){
         var option_panel_width = canvas.width * 74 / 490;
         var option_panel_height = canvas.height * 15 / 275;
         var option_panel_x_offset = canvas.width * 3 / 490;
-        var option_panel_y_offset = canvas.height - canvas.height * 3 / 275;
-        context.beginPath();
-        context.moveTo(option_panel_x_offset, option_panel_y_offset);
-        context.lineTo(option_panel_x_offset + option_panel_width, option_panel_y_offset);
-        context.lineTo(option_panel_x_offset + option_panel_width, option_panel_y_offset - option_panel_height);
-        context.lineTo(option_panel_x_offset, option_panel_y_offset - option_panel_height);
-        context.fillStyle = board_inner_pattern;
-        context.fill();
+        var option_panel_y_offset = canvas.height - canvas.height * 3 / 275 - option_panel_height;
+    	context.drawImage(options_image,
+                      	  dx=option_panel_x_offset, dy=option_panel_y_offset,
+                          dWidth=option_panel_width, dHeight=option_panel_height);
 
         //---------------------------------------
         // Draw the four name tags.
@@ -458,10 +644,17 @@ window.onload = function(){
         var tag_width = tag_length / 9;
         var lr_player_x_offset = canvas.width / 2.0 - 1.2 * board_side_length;
         var lr_player_y_offset = (canvas.height - tag_length) / 2.0;
+
+        var left_x_offset = (canvas.height - board_side_length) / 2.0 + tag_length / 2;
+        var left_y_offset = canvas.width / 2.0 - 1.2 * board_side_length + tag_width / 2;
+
+        var right_x_offset = (canvas.height - board_side_length) / 2.0 + tag_length / 2;
+        var right_y_offset = canvas.width / 2.0 + 1.2 * board_side_length - tag_width / 2;
+
         var tb_player_x_offset = (canvas.width - tag_length) / 2.0;
-        var tb_player_y_offset = canvas.height / 2.0 - 1.05 * board_side_length
+        var tb_player_y_offset = canvas.height / 2.0 - 1.0 * board_side_length
         var font_size = 0.7 * tag_width;
-        context.font = 'bold ' + font_size.toString() + 'px monospace';
+        context.font = 'bold ' + font_size.toString() + 'px Brush Script MT';
         context.textAlign="center";
         context.textBaseline = "middle";;
 
@@ -474,11 +667,11 @@ window.onload = function(){
         context.closePath();
         context.fillStyle = nametag_pattern;
         context.strokeStyle = nametag_border_pattern;
-        context.lineWidth = 5;
+        context.lineWidth = 2;
         context.fill();
         context.stroke();
         context.fillStyle = chalk_pattern;
-        write_vertical_text(left_player_id, lr_player_x_offset + tag_width / 2, lr_player_y_offset);
+        write_vertical_text(left_player_id, left_x_offset, left_y_offset);
 
         // Draw the right players name tag.
         context.beginPath();
@@ -489,11 +682,11 @@ window.onload = function(){
         context.closePath();
         context.fillStyle = nametag_pattern;
         context.strokeStyle = nametag_border_pattern;
-        context.lineWidth = 5;
+        context.lineWidth = 2;
         context.fill();
         context.stroke();
         context.fillStyle = chalk_pattern;
-        write_vertical_text(right_player_id, canvas.width - lr_player_x_offset + tag_width / 2, lr_player_y_offset + tag_width);
+        write_vertical_text(right_player_id, right_x_offset, right_y_offset);
 
         // Draw the top players name tag.
         context.beginPath();
@@ -504,7 +697,7 @@ window.onload = function(){
         context.closePath();
         context.fillStyle = nametag_pattern;
         context.strokeStyle = nametag_border_pattern;
-        context.lineWidth = 5;
+        context.lineWidth = 2;
         context.fill();
         context.stroke();
         context.fillStyle = chalk_pattern;
@@ -520,13 +713,19 @@ window.onload = function(){
         context.closePath();
         context.fillStyle = nametag_pattern;
         context.strokeStyle = nametag_border_pattern;
-        context.lineWidth = 5;
+        context.lineWidth = 2;
         context.fill();
         context.stroke();
         context.fillStyle = chalk_pattern;
         context.fillText(bottom_player_id[0] + ". " + bottom_player_id.slice(1), tb_player_x_offset + tag_length / 2,
                          canvas.height - tb_player_y_offset - tag_width / 2);
 
+
+        // Draw the cards.
+        draw_top_side_cards(24, 8);
+        draw_bottom_side_cards(22, 3);
+        draw_left_side_cards(22, 3);
+        draw_right_side_cards(22, 3);
 
 	}, 33*5);
 }
